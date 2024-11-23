da7e896d35418f4cab2b967726b6411d006d8db5
zeyadgamal15 <2401606@csi.edu.eg>
2024-11-23 20:11:55 +0200

    Add hello function

---
+++ functions/hello.js
@@ -0,0 +1,8 @@
+// functions/hello.js
+exports.handler = async function(event, context) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Hello from Netlify Function!" }),
+    };
+  };
+  
